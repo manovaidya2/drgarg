cff3bcf818d916cf06b08672aecc5242835ad5bb
Abhishek Kumar <abhishek8579013@gmail.com>
2026-01-15 17:17:36 +0530

    update

--- frontend/src/api/axiosInstance.js
+++ frontend/src/api/axiosInstance.js
@@ -1,7 +1,8 @@
 import axios from "axios";
 
 const axiosInstance = axios.create({
-  baseURL: "http://localhost:5003/api",
+  // baseURL: "http://localhost:5003/api",
+  baseURL: "https://api.drankushgarg.com/api", // Production URL
   headers: {
     "Content-Type": "application/json",
   },
